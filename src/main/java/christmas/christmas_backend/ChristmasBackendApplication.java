e9da85835c6abd2d329e66ca6091eb372a7eeb22
나의찬 <cab07036@naver.com>
2025-12-20 17:53:54 +0900

    first commit

---
+++ src/main/java/christmas/christmas_backend/ChristmasBackendApplication.java
@@ -0,0 +1,13 @@
+package christmas.christmas_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChristmasBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChristmasBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/christmas/christmas_backend/ChristmasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package christmas.christmas_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChristmasBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
